43542de496c8475b64830db2a3948076459150a3
manish gandotra <manishgandotra74@gmail.com>
2020-07-23 15:09:56 +0530

    changes

--- users-ms/server.js
+++ users-ms/server.js
@@ -1,6 +1,11 @@
 const express = require ('express');
 const app = express();
 
+
+// routes 
+
+
+// ..testing 
 app.get('/test', function(req, res){
     res.status(200).send({message : 'Testing Success'})
 })
